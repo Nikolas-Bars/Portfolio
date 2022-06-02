c147ed3f655efb101adf06513531882025e92f8e
Nikolas-Bars <kasp2409@mail.ru>
2022-06-02 14:16:29 +0300

    added particles

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,12 +1,110 @@
 import React from 'react';
 import s from './Main.module.scss'
+import Particles from "react-tsparticles";
+import {loadFull}  from "tsparticles";
 
 
 const Main = () => {
+
+    const particlesInit = async (main) => {
+        console.log(main);
+
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(main);
+    };
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+
+
+    const particlesOptions = {
+        particles: {
+                number:{
+                    value: 80,
+                    density: {
+                        enable: true,
+                        value_area: 800
+                    }
+                }
+        }
+    }
+
     return (
+<div className={s.mainBlock}>
 
-        <div className={s.mainBlock}>
+    <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
 
+            fpsLimit: 120,
+            interactivity: {
+                events: {
+                    onClick: {
+                        enable: true,
+                        mode: "push",
+                    },
+
+                    resize: false,
+                },
+                modes: {
+                    push: {
+                        quantity: 1,
+                    },
+                    repulse: {
+                        distance: 100,
+                        duration: 0.4,
+                    },
+                },
+            },
+            particles: {
+                color: {
+                    value: "#ffffff",
+                },
+                links: {
+                    color: "#ffffff",
+                    distance: 150,
+                    enable: true,
+                    opacity: 0.2,
+                    width: 1,
+                },
+                collisions: {
+                    enable: true,
+                },
+                move: {
+                    direction: "none",
+                    enable: true,
+                    outModes: {
+                        default: "bounce",
+                    },
+                    random: false,
+                    speed: 2,
+                    straight: false,
+                },
+                number: {
+                    density: {
+                        enable: true,
+                        area: 800,
+                    },
+                    value: 80,
+                },
+                opacity: {
+                    value: 0.5,
+                },
+                shape: {
+                    type: "circle",
+                },
+                size: {
+                    value: { min: 0.5, max: 2 },
+                },
+            },
+            detectRetina: true,
+        }}
+    />
             <div className={s.container}>
 
                <div className={s.intro}>
